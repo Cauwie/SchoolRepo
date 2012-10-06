5bc7d3c5c98a83e9edbafd68054382b36782452f
Zenzor <eaglewing_89@hotmail.com>
2012-10-06 23:13:58 +0200

    Adds tests that fails

--- Enterprise/Innlevering1/src/test/java/no/nith/pg560/application/TechnologyServiceBeanTest.java
+++ Enterprise/Innlevering1/src/test/java/no/nith/pg560/application/TechnologyServiceBeanTest.java
@@ -1,7 +1,9 @@
 package no.nith.pg560.application;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import no.nith.pg560.domain.Technology;
@@ -26,20 +28,42 @@ public class TechnologyServiceBeanTest {
     	technologyServiceBean = new TechnologyServiceBean();
         MockitoAnnotations.initMocks(this);
     }
-//
-//    @Test
-//    public void readUsers() throws Exception {
-//    	when(technologyJpaRepository.getTechnologies()).thenReturn(buildTechnologies());
-//        Technology technology = technologyServiceBean.getUser("Ola");
-//        assertNotNull(technology);
-//    }	
-//    
-//    private Technology buildTechnologies() {
-//    	List<Technology> technologies;
-//    	Technology technology = new Technology();
-//    	technology.setDescription("test");
-//    	
-//    	return technology;
-//    }
 
+    @Test
+    public void getAllTechnologies() throws Exception {
+    	when(technologyJpaRepository.getTechnologies()).thenReturn(buildTechnologies());
+    	List<Technology> technologies = technologyServiceBean.getTechnologyList();
+        Technology technology = technologies.get(0);
+        assertNotNull(technology);
+        assertEquals(buildTechnologies(), technologyServiceBean.getTechnologyList());
+    }
+    
+    @Test
+    public void searchForTechnologies() throws Exception {
+    	when(technologyJpaRepository.searchTechnologies("testAcro", "testVersion", "testJSR", "testDescription")).thenReturn(buildTechnologies());
+        Technology technology = technologyServiceBean.getTechnologyList().get(0);
+        assertNotNull(technology);
+        assertEquals(buildTechnologies(), technologyServiceBean.getTechnologyList());
+    }
+    
+    private List<Technology> buildTechnologies() {
+    	List<Technology> technologies = new ArrayList<Technology>();
+    	
+    	Technology technology = new Technology();
+    	technology.setAcronyms("testAcro");
+    	technology.setVersion("testVersion");
+    	technology.setJsr("testJSR");
+    	technology.setDescription("testDescription");
+    	
+    	Technology technology2 = new Technology();
+    	technology.setAcronyms("testAcro2");
+    	technology.setVersion("testVersion2");
+    	technology.setJsr("testJSR2");
+    	technology.setDescription("testDescription2");
+
+    	technologies.add(technology);
+    	technologies.add(technology2);
+    	
+    	return technologies;
+    }
 }
--- Enterprise/Innlevering1/src/test/java/no/nith/pg560/common/CommonInfrastructureIT.java
+++ Enterprise/Innlevering1/src/test/java/no/nith/pg560/common/CommonInfrastructureIT.java
@@ -1,9 +1,14 @@
 package no.nith.pg560.common;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+
+import no.nith.pg560.domain.Technology;
 
 import org.junit.After;
 import org.junit.Before;
@@ -21,11 +26,43 @@ public class CommonInfrastructureIT {
         entityManager = createEntityManager("pg560-test");
         tr = entityManager.getTransaction();
         tr.begin();
+        buildTechnologies();
     }
 
     private EntityManager createEntityManager(String persistenceUnit) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory(persistenceUnit, System.getProperties());
         return emf.createEntityManager();
+    }
+
+	protected List<Technology> buildTechnologies() {
+		
+		List<Technology> technologies = new ArrayList<Technology>();
+    	
+    	Technology technology = new Technology();
+    	technology.setAcronyms("testAcro");
+    	technology.setVersion("testVersion");
+    	technology.setJsr("testJSR");
+    	technology.setDescription("testDescription");
+    	
+    	Technology technology2 = new Technology();
+    	technology2.setAcronyms("testAcro2");
+    	technology2.setVersion("testVersion2");
+    	technology2.setJsr("testJSR2");
+    	technology2.setDescription("testDescription2");
+
+    	technologies.add(technology);
+    	technologies.add(technology2);
+    	
+//    	getEntityManager().createQuery("insert into PG560_JEE_TECHNOLOGY" 
+//    								+ "(acronyms, version, jsr, description) "
+//    								+ "values(" 
+//    								+ " '" + technology.getAcronyms() + "', '" 
+//    								+ technology.getVersion() + "', '" 
+//    								+ technology.getJsr() + "', '" 
+//    								+ technology.getDescription() + "');").executeUpdate();
+//        tr.commit();
+        
+        return technologies;
     }
 
     protected EntityManager getEntityManager() {
