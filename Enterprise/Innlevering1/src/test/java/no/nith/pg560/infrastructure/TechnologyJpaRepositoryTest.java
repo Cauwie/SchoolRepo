86cdd4712baf0d1f5235b72259525dbb77e21e21
Zenzor <eaglewing_89@hotmail.com>
2012-10-11 11:06:09 +0200

    modified:   Enterprise/Innlevering1/src/main/java/no/nith/pg560/infrastructure/TechnologyJpaRepository.java 	modified:   Enterprise/Innlevering1/src/main/resources/database.sql 	modified:   Enterprise/Innlevering1/src/test/java/no/nith/pg560/infrastructure/TechnologyJpaRepositoryTest.java

--- Enterprise/Innlevering1/src/test/java/no/nith/pg560/infrastructure/TechnologyJpaRepositoryTest.java
+++ Enterprise/Innlevering1/src/test/java/no/nith/pg560/infrastructure/TechnologyJpaRepositoryTest.java
@@ -5,21 +5,37 @@ import java.util.List;
 import no.nith.pg560.common.CommonInfrastructureIT;
 import no.nith.pg560.domain.Technology;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class TechnologyJpaRepositoryTest extends CommonInfrastructureIT {
+	
+	private TechnologyJpaRepository technologyRepo;
+	
+	@Before
+	public void setup() {
+		technologyRepo = new TechnologyJpaRepository(getEntityManager());
+	}
+	
 
 	@Test
 	public void testGetTechnologies() {
-		TechnologyJpaRepository technologyRepo = new TechnologyJpaRepository(getEntityManager());
 		List<Technology> technologyResult = technologyRepo.getTechnologies();
 		assertEquals("Java EE", technologyResult.get(0).getAcronyms());
 		assertEquals("6", technologyResult.get(0).getVersion());
 		assertEquals("316", technologyResult.get(0).getJsr());
 		assertEquals("This JSR is to develop Java EE 6, a release of the Java Platform, Enterprise Edition targeted to ship in 2008.", technologyResult.get(0).getDescription());
 	}
+	
+//	@Test
+//	public void testSearchTechnologies() {
+//		List<Technology> technologyResult = technologyRepo.searchTechnologies(acronyms, version, jsf, description);
+//		
+//		
+//		
+//	}
 	
 //	@Test
 //	public void testGetUser() {
