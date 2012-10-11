ade67eea449b2a709e02a64aa4fb2c3d9a34b927
Zenzor <eaglewing_89@hotmail.com>
2012-10-11 11:58:34 +0200

    Adds a repositorytest

--- Enterprise/Innlevering1/src/test/java/no/nith/pg560/infrastructure/TechnologyJpaRepositoryTest.java
+++ Enterprise/Innlevering1/src/test/java/no/nith/pg560/infrastructure/TechnologyJpaRepositoryTest.java
@@ -1,5 +1,6 @@
 package no.nith.pg560.infrastructure;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import no.nith.pg560.common.CommonInfrastructureIT;
@@ -12,11 +13,13 @@ import static org.junit.Assert.*;
 
 public class TechnologyJpaRepositoryTest extends CommonInfrastructureIT {
 	
+	private List<Technology> technologyList;
 	private TechnologyJpaRepository technologyRepo;
 	
 	@Before
 	public void setup() {
 		technologyRepo = new TechnologyJpaRepository(getEntityManager());
+		buildTechnologyList();
 	}
 	
 
@@ -29,24 +32,26 @@ public class TechnologyJpaRepositoryTest extends CommonInfrastructureIT {
 		assertEquals("This JSR is to develop Java EE 6, a release of the Java Platform, Enterprise Edition targeted to ship in 2008.", technologyResult.get(0).getDescription());
 	}
 	
-//	@Test
-//	public void testSearchTechnologies() {
-//		List<Technology> technologyResult = technologyRepo.searchTechnologies(acronyms, version, jsf, description);
-//		
-//		
-//		
-//	}
+	@Test
+	public void testSearchTechnologies() {
+		List<Technology> technologyResult = technologyRepo.searchTechnologies("Java", "6", "316", "This JSR");
+		assertEquals("Java EE", technologyResult.get(0).getAcronyms());
+		assertEquals("6", technologyResult.get(0).getVersion());
+		assertEquals("316", technologyResult.get(0).getJsr());
+		assertEquals("This JSR is to develop Java EE 6, a release of the Java Platform, Enterprise Edition targeted to ship in 2008.", technologyResult.get(0).getDescription());
+	}
 	
-//	@Test
-//	public void testGetUser() {
-//		UserJpaRepository userRepo = new UserJpaRepository(getEntityManager());
-//		User userResult = userRepo.getUser("tonnyg");
-//		assertEquals(userResult.getName(), "Tonny Gundersen");
-//		assertEquals(userResult.getCity(), "Oslo");
-//		assertEquals(userResult.getCountry(), "Norway");
-//
-//		userResult = userRepo.getUser("Finnes ikke");
-//		assertNull(userResult);
-//	}
-
+	private List<Technology> buildTechnologyList() {
+		List<Technology> technologyList = new ArrayList<Technology>();
+		
+		Technology technology = new Technology();
+    	technology.setAcronyms("Java EE");
+    	technology.setVersion("6");
+    	technology.setJsr("316");
+    	technology.setDescription("This JSR is to develop Java EE 6, a release of the Java Platform, Enterprise Edition targeted to ship in 2008.");
+    	
+    	technologyList.add(technology);
+    	
+    	return technologyList;
+	}
 }
