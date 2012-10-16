646c5e30dc996c2f4729387a53fc86b7854131fb
David Emanuelsen <davidemanuelsen@hotmail.com>
2012-10-16 21:09:42 +0200

    Fixed PrimeFaces export function!

--- Enterprise/Innlevering1/src/main/java/no/nith/pg560/interfaces/web/Export.java
+++ Enterprise/Innlevering1/src/main/java/no/nith/pg560/interfaces/web/Export.java
@@ -4,10 +4,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Serializable;
  
-import javax.enterprise.context.Conversation;
 import javax.enterprise.context.ConversationScoped;
 import javax.faces.context.FacesContext;
-import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.http.HttpServletResponse;
  
