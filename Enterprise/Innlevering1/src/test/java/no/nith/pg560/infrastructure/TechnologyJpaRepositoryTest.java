776050ece968d7c1198edd2276a7bfb00c9fa4e4
David Raphael Emanuelsen <david.emanuelsen@outlook.com>
2012-10-17 13:16:50 +0200

    Fixed mocking on JpaRepo

--- Enterprise/Innlevering1/src/test/java/no/nith/pg560/infrastructure/TechnologyJpaRepositoryTest.java
+++ Enterprise/Innlevering1/src/test/java/no/nith/pg560/infrastructure/TechnologyJpaRepositoryTest.java
@@ -3,40 +3,38 @@ package no.nith.pg560.infrastructure;
 import java.util.ArrayList;
 import java.util.List;
 
-import no.nith.pg560.common.CommonInfrastructureIT;
 import no.nith.pg560.domain.Technology;
 
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-public class TechnologyJpaRepositoryTest extends CommonInfrastructureIT {
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
+
+public class TechnologyJpaRepositoryTest {
 	
-	private TechnologyJpaRepository technologyRepo;
+	@Mock
+	private TechnologyJpaRepository technologyJpaRepository;
 	
 	@Before
 	public void setup() {
-		technologyRepo = new TechnologyJpaRepository(getEntityManager());
-		buildTechnologyList();
-	}
-
-	@Test
-	public void testGetTechnologies() {
-		List<Technology> technologyResult = technologyRepo.getTechnologies();
-		assertEquals("Java EE", technologyResult.get(0).getAcronyms());
-		assertEquals("6", technologyResult.get(0).getVersion());
-		assertEquals("316", technologyResult.get(0).getJsr());
-		assertEquals("This JSR is to develop Java EE 6, a release of the Java Platform, Enterprise Edition targeted to ship in 2008.", technologyResult.get(0).getDescription());
+		MockitoAnnotations.initMocks(this);
 	}
 	
 	@Test
 	public void testSearchTechnologies() {
-		List<Technology> technologyResult = technologyRepo.searchTechnologies("Java", "6", "316", "This JSR");
-		assertEquals("Java EE", technologyResult.get(0).getAcronyms());
-		assertEquals("6", technologyResult.get(0).getVersion());
-		assertEquals("316", technologyResult.get(0).getJsr());
-		assertEquals("This JSR is to develop Java EE 6, a release of the Java Platform, Enterprise Edition targeted to ship in 2008.", technologyResult.get(0).getDescription());
+		when(technologyJpaRepository.searchTechnologies("Java", "6", "316", "This JSR")).thenReturn(buildTechnologyList());
+		
+		Technology technology = technologyJpaRepository.searchTechnologies("Java", "6", "316", "This JSR").get(0);
+		Technology technology2 = buildTechnologyList().get(0);
+		
+		assertEquals(technology.getAcronyms(), technology2.getAcronyms());
+		assertEquals(technology.getVersion(), technology2.getVersion());
+		assertEquals(technology.getJsr(), technology2.getJsr());
+		assertEquals(technology.getDescription(), technology2.getDescription());
 	}
 	
 	private List<Technology> buildTechnologyList() {
