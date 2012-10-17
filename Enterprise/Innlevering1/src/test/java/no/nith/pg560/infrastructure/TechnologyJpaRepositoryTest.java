c8fac6d0af89ad7dea7db98b141768756ef39cb2
Zenzor <eaglewing_89@hotmail.com>
2012-10-17 15:13:18 +0200

    modified:   Enterprise/Innlevering1/src/test/java/no/nith/pg560/infrastructure/TechnologyJpaRepositoryTest.java

--- Enterprise/Innlevering1/src/test/java/no/nith/pg560/infrastructure/TechnologyJpaRepositoryTest.java
+++ Enterprise/Innlevering1/src/test/java/no/nith/pg560/infrastructure/TechnologyJpaRepositoryTest.java
@@ -3,6 +3,10 @@ package no.nith.pg560.infrastructure;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
 import no.nith.pg560.domain.Technology;
 
 import org.junit.Before;
@@ -17,16 +21,28 @@ import static org.mockito.Mockito.when;
 public class TechnologyJpaRepositoryTest {
 	
 	@Mock
+	private Query query;
+	
+	@Mock
+	private EntityManager em;
+	
+	@Mock
 	private TechnologyJpaRepository technologyJpaRepository;
 	
 	@Before
 	public void setup() {
+		em = Persistence.createEntityManagerFactory("pg560-test", System.getProperties()).createEntityManager();
+		query = em.createQuery("SELECT t from Technology t");
 		MockitoAnnotations.initMocks(this);
 	}
 	
 	@Test
 	public void testSearchTechnologies() {
-		when(technologyJpaRepository.searchTechnologies("Java", "6", "316", "This JSR")).thenReturn(buildTechnologyList());
+		
+		technologyJpaRepository = new TechnologyJpaRepository(em);
+		when(technologyJpaRepository.getEntityManager()).thenReturn(em);
+		when(em.createQuery(null)).thenReturn(query);
+		when(query.getResultList()).thenReturn(buildTechnologyList());
 		
 		Technology technology = technologyJpaRepository.searchTechnologies("Java", "6", "316", "This JSR").get(0);
 		Technology technology2 = buildTechnologyList().get(0);
