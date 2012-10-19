2bfcc3ffb64c5b470cb9975750a97e7c4a22bcf3
David Emanuelsen <davidemanuelsen@hotmail.com>
2012-10-19 19:53:37 +0200

    Random

---
+++ Enterprise/Innlevering1/src/main/java/no/nith/pg560/common/PageNavigation.java
@@ -0,0 +1,7 @@
+package no.nith.pg560.common;
+public final class PageNavigation {
+    public static final String INDEX_PAGE = "index";
+    public static final String ERROR_PAGE = "errorPage";
+
+    private PageNavigation() {}
+}
